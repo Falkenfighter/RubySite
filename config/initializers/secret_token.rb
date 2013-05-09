823c7014365f1d1b5104afa9223ae57e8fc387ef
Justin Graham <Justin.af.graham@gmail.com>
2013-05-08 19:05:04 -0700

    Adding init files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JustinandChareesa::Application.config.secret_token = 'cd92d467f1e56d1a7aa492475bbe5c08f61db913612a3e99e6048484e205da76140828ae649792263ccbf8a276478b5fd4816be8b2aa5b02764eb2ef31cc5c95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JustinandChareesa::Application.config.session_store :cookie_store, key: '_JustinandChareesa_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JustinandChareesa::Application.config.session_store :active_record_store
